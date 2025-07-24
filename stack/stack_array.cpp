8d0340b273a317ef8448223b5b88b75eaff87837
gjsoy24 <gour.joy24@gmail.com>
2025-07-24 10:03:23 +0600

    added  stack

--- stack/stack_array.cpp
+++ stack/stack_array.cpp
@@ -1,8 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class myStack
+{
+public:
+  vector<int> v;
+
+  void push(int val)
+  {
+    v.push_back(val);
+  }
+
+  void pop()
+  {
+    v.pop_back();
+  }
+
+  int top()
+  {
+    return v.back();
+  }
+
+  int size()
+  {
+    return v.size();
+  }
+
+  bool isEmpty()
+  {
+    return v.empty();
+  }
+
+};
+
 int main()
 {
+  myStack nums;
+
+  nums.push(10);
+  nums.push(20);
+  nums.push(30);
+  // nums.push(40);
+
+  cout << nums.top() << endl;
 
   return 0;
-}
+};
