e009d68b80629b96a2761a00b34de31b623c4277
gjsoy24 <gour.joy24@gmail.com>
2025-08-03 11:38:29 +0600

    ccc

---
+++ tree/count_leaf_node.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *input_tree()
+{
+  int val;
+  cin >> val;
+
+  Node *root;
+
+  if (val == -1)
+  {
+    root = NULL;
+    return root;
+  }
+  else
+  {
+    root = new Node(val);
+  }
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    // ber kore ana
+    Node *p = q.front();
+    q.pop();
+
+    // work with that node.
+    int l, r;
+    cin >> l >> r;
+
+    Node *myLeft, *myRight;
+
+    if (l == -1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+
+    if (r == -1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+
+    p->left = myLeft;
+    p->right = myRight;
+
+    // push the childrens
+    if (p->left)
+      q.push(p->left);
+
+    if (p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
+int count_leaf_node(Node *root)
+{
+  if (!root)
+    return 0;
+
+  if (root->left == NULL && root->right == NULL)
+  {
+    return 1;
+  }
+
+  int l = count_leaf_node(root->left);
+  int r = count_leaf_node(root->right);
+
+  return l + r;
+}
+
+int main()
+{
+
+  Node *root = input_tree();
+
+  int count = count_leaf_node(root);
+  cout << count << endl;
+
+  return 0;
+}
