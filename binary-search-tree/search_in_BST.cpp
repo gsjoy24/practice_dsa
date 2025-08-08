543a0278879e0820aece21381d582e1fff9c859d
gjsoy24 <gour.joy24@gmail.com>
2025-08-08 14:44:35 +0600

    cc

--- binary-search-tree/search_in_BST.cpp
+++ binary-search-tree/search_in_BST.cpp
@@ -16,6 +16,58 @@ public:
   }
 };
 
+Node *input_tree()
+{
+  int val;
+  cin >> val;
+
+  Node *root;
+
+  if (val == -1)
+  {
+    root = NULL;
+    return root;
+  }
+  else
+  {
+    root = new Node(val);
+  }
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    Node *p = q.front();
+    q.pop();
+
+    int l, r;
+    cin >> l >> r;
+
+    Node *myLeft, *myRight;
+
+    if (l == -1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+
+    if (r == -1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+
+    p->left = myLeft;
+    p->right = myRight;
+
+    if (p->left)
+      q.push(p->left);
+
+    if (p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
 int main()
 {
 
