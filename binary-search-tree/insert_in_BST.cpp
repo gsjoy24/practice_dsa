38e20b384eb4d480e3ce6ba38e2de110486f4366
gjsoy24 <gour.joy24@gmail.com>
2025-08-10 17:32:55 +0600

    cc

---
+++ binary-search-tree/insert_in_BST.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *input_tree()
+{
+  int val;
+  cin >> val;
+
+  Node *root;
+
+  if (val == -1)
+  {
+    root = NULL;
+    return root;
+  }
+  else
+  {
+    root = new Node(val);
+  }
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    Node *p = q.front();
+    q.pop();
+
+    int l, r;
+    cin >> l >> r;
+
+    Node *myLeft, *myRight;
+
+    if (l == -1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+
+    if (r == -1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+
+    p->left = myLeft;
+    p->right = myRight;
+
+    if (p->left)
+      q.push(p->left);
+
+    if (p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
+bool insert(Node *root, int val)
+{
+
+
+}
+
+int main()
+{
+  Node *root = input_tree();
+  int val;
+  cin >> val;
+
+  if (insert(root, val))
+    cout << "Found" << endl;
+  else
+    cout << "Not Found" << endl;
+
+  return 0;
+}
