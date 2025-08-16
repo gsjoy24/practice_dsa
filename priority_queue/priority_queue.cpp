077e58521e1e34e21beca45c232c5ab54eca049b
gjsoy24 <gour.joy24@gmail.com>
2025-08-16 10:57:02 +0600

    c

--- priority_queue/priority_queue.cpp
+++ priority_queue/priority_queue.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main()
 {
+  // !max priority
   priority_queue<int> pq;
   pq.push(10);
   pq.push(5);
   pq.push(30);
-  cout << pq.top() << endl;
   pq.push(100);
+  cout << pq.top() << endl;
+
+  // !min priority
+  priority_queue<int, vector<int>, greater<int>> min_pq;
+  min_pq.push(10);
+  min_pq.push(5);
+  min_pq.push(30);
+  min_pq.push(100);
+  cout << min_pq.top() << endl;
   return 0;
 }
